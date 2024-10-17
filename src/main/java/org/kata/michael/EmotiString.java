d4b5b21e509879ce6cc493e20e6c1ea3f1a0be21
mik-rzo <michael.panong3@gmail.com>
2024-10-17 12:01:22 +0100

    completed EmotiString kata

--- src/main/java/org/kata/michael/EmotiString.java
+++ src/main/java/org/kata/michael/EmotiString.java
@@ -1,5 +1,9 @@
 package org.kata.michael;
 
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmotiString {
 
   /**
@@ -14,7 +18,35 @@ public class EmotiString {
    * @return {@code String} a string with the specified words replaced by emoticons
    */
   public String emotify(String sentence) {
-    return null;
+    // create a map of words with their corresponding emoticons
+    HashMap<String, String> emoticonMap = new HashMap<>();
+    emoticonMap.put("smile", ":)");
+    emoticonMap.put("grin", ":D");
+    emoticonMap.put("sad", ":(");
+    emoticonMap.put("mad", ":@");
+
+    // create regex pattern
+    Pattern pattern = Pattern.compile("(" + String.join("|", emoticonMap.keySet()) + ")");
+
+    // create matcher to perform match operations
+    Matcher matcher = pattern.matcher(sentence);
+
+    // create stringbuilder to store result
+    StringBuilder sb = new StringBuilder();
+
+    // replace words in sentence with emoticon
+    while (matcher.find()) {
+      // get the matched word
+      String match = matcher.group();
+
+      // use matched word to replace with corresponding emoticon from hashmap
+      matcher.appendReplacement(sb, emoticonMap.get(match));
+    }
+
+    // copies remainder of the input sentence
+    matcher.appendTail(sb);
+
+    return sb.toString();
   }
 
 }
