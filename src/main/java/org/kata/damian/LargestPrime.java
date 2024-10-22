e5b9f71e7c5980b1b0a401a059c5387461e3a123
Damian <damiangildea12@gmail.com>
2024-10-22 15:01:23 +0100

    Completed largestPrime kata eventually

--- src/main/java/org/kata/damian/LargestPrime.java
+++ src/main/java/org/kata/damian/LargestPrime.java
@@ -16,9 +16,41 @@ public class LargestPrime {
      * 1 and itself.
      */
 
-    public int getLargestPrime(int n) {
+    public static int getLargestPrime(int n) {
+        if (n < 2) {
+            return -1;
+        }
+
+        // Start from n and go down to 2 - to find first/largest prime
+        for (int i = n; i >= 2; i--) {
+            if (isPrime(i)) {
+                return i;
+            }
+        }
         return -1;
     }
-}
 
+    private static boolean isPrime(int num) {
+        if (num < 2) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+
+        // Check for factors among odd numbers starting from 3.
+        // We only need to check odd numbers (i) because all even numbers greater than 2
+        // have already been ruled out as potential factors.
+        // The loop continues as long as i is less than or equal to the square root of num.
+        // This is important because if num has any factors, at least one of them
+        // must be less than or equal to the square root of num.
+        // If no factors are found by the time we reach the square root,
+        // we can conclude that num is prime.
+        for (int i = 3; i <= Math.sqrt(num); i += 2) {
+            // If i divides num, then num is not prime
+            if (num % i == 0) return false;
+        }
+
+
+        // If there are no factors found, return true
+        return true;
+    }
+}
 
