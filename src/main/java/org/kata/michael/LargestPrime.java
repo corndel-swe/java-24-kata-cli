283d431d1767a620fd3b82a9bcc5e750c8710e20
mik-rzo <michael.panong3@gmail.com>
2024-10-25 11:57:15 +0100

    completed LargestPrime kata

--- src/main/java/org/kata/michael/LargestPrime.java
+++ src/main/java/org/kata/michael/LargestPrime.java
@@ -2,23 +2,43 @@ package org.kata.michael;
 
 public class LargestPrime {
 
-    /**
-     * Finds the largest prime number less than or equal to a given integer.
-     * <p>
-     * The function takes an integer input and returns the largest prime number that is less than or equal
-     * to the specified integer. If the input integer is less than 2, the function will return -1, as there
-     * are no prime numbers below 2. If the input integer itself is prime, the function will return that
-     * integer. The implementation efficiently checks each number starting from the given integer and works
-     * downwards, stopping at the first prime number it encounters.
-     * <p>
-     * A prime number is defined as a natural number greater than 1 that cannot be formed by multiplying
-     * two smaller natural numbers. This means that a prime number has exactly two distinct positive divisors:
-     * 1 and itself.
-     */
+  /**
+   * Finds the largest prime number less than or equal to a given integer.
+   * <p>
+   * The function takes an integer input and returns the largest prime number that is less than or equal
+   * to the specified integer. If the input integer is less than 2, the function will return -1, as there
+   * are no prime numbers below 2. If the input integer itself is prime, the function will return that
+   * integer. The implementation efficiently checks each number starting from the given integer and works
+   * downwards, stopping at the first prime number it encounters.
+   * <p>
+   * A prime number is defined as a natural number greater than 1 that cannot be formed by multiplying
+   * two smaller natural numbers. This means that a prime number has exactly two distinct positive divisors:
+   * 1 and itself.
+   */
 
-    public int getLargestPrime(int n) {
-        return -1;
+  public int getLargestPrime(int n) {
+    if (n < 2) {
+      return -1;
     }
+    for (int i = n; i > 1; i--) {
+      if (isPrime(i)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  private boolean isPrime(int num) {
+    if (num == 2 || num == 3) {
+      return true;
+    }
+    for (int i = 2; i * i <= num; i++) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 
